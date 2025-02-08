9fa475b031837ea8a2f8f740c40ec170122f991a
cristian <cchura.cpc@gmail.com>
2025-02-08 00:08:56 -0400

    docker solucionado

--- frontend_proyecto_vue_laravel/src/services/api.service.js
+++ frontend_proyecto_vue_laravel/src/services/api.service.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const BASE_URL = "http://3.91.252.203";
+export const BASE_URL = "http://127.0.0.1:8000";
 export const BASE_URL_API = `${BASE_URL}/api`;
 
 export default function Api(){
